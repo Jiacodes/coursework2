85cbb613bfdc998e428288f9a160214858abef5e
Jiacodes <atwijukaji@gmail.com>
2025-10-30 04:35:40 +0300

    commit3.1
    Added 4 variables, a static block to initialize 2 of them and a constructor to initialize the other 2.

--- hospital/taskAutomation/patientInfo.java
+++ hospital/taskAutomation/patientInfo.java
@@ -1,4 +1,18 @@
 package taskAutomation;
 
 public class patientInfo {
+    private static String hospitalName;
+    private static String ward;
+    private String patientName;
+    private int patientBedNo;
+
+    static {
+        hospitalName = "GET WELL SOON CHILDREN'S HOSPITAL";
+        ward = "Paediatrics ward.";
+    }
+
+    public patientInfo(String Pname, int PBedNo) {
+        patientName = Pname;
+        patientBedNo = PBedNo;
+    }
 }
